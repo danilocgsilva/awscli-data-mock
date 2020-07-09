8b95aef24cb611def871d982c9f2bbdaaa64aa23
Danilo Silva <danilocgsilva@gmail.com>
2020-07-09 18:23:07 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name="awscli-data-mock",
+    version=VERSION,
+    description="Mocks json data from aws cli responses for testting and privacy porpouses",
+    long_description_content_type="text/markdown",
+    long_description=readme(),
+    keywords="test mock privacy aws cli",
+    url="https://github.com/danilocgsilva/awscli-data-mock",
+    author="Danilo Carlos de Góes Silva",
+    author_email="contact@danilocgsilva.me",
+    packages=["awsclimock"],
+    entry_points={"console_scripts": ["awsclimock.__main__:main"],},
+    include_package_data=True
+)
