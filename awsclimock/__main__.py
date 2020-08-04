eadca990cf8cd10900f846cd317c590b24ab0974
Danilo Silva <contact@danilocgsilva.me>
2020-08-04 20:09:31 -0300

    Corrected output to a more suitable json format
    It was printing the dictionary directly. But now uses the dumps. Its
    corrected the problema ou output beign written with single quotes
    tha doubles ones. This corrected some problemas in the script usage

--- awsclimock/__main__.py
+++ awsclimock/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from awsclimock.helpers import get_command_to_mock
 from awsapimock.Security_Group_Data_Generator \
     import Security_Group_Data_Generator
@@ -12,16 +13,22 @@ from awsapimock.RDS_Data_Generator \
 def main():
     aws_command = get_command_to_mock()
 
+    data = None
+
     if aws_command == 'describe-security-groups':
-        print(Security_Group_Data_Generator().generate())
+        data = Security_Group_Data_Generator().generate()
     elif aws_command == 'describe-instances':
-        print(Instance_Data_Generator().generate())
+        data = Instance_Data_Generator().generate()
     elif aws_command == 'describe-regions':
-        print(Regions_Data_Generator().generate())
+        data = Regions_Data_Generator().generate()
     elif aws_command == 'describe-db-instances':
-        print(RDS_Data_Generator().generate())
-    else:
+        data = RDS_Data_Generator().generate()
+    
+    if data == None:
         print('I dont know this command yet!')
+        exit()
+
+    print(json.dumps(data))
 
 
 if __name__ == '__main__':
